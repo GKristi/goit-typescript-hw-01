5aef829ec8226fa99fb9ba7b178bda0d6f389272
Kristina Holub <christigolub@gmail.com>
2023-10-30 08:22:39 +0200

    complete task

---
+++ dist/concatenation.js
@@ -0,0 +1,11 @@
+function concatenation(firstWord, secondWord) {
+    console.log(`${firstWord} ${secondWord}`);
+}
+export { concatenation };
+let notSure = 4;
+notSure = "maybe a string instead";
+notSure = false;
+notSure = {};
+let num;
+notSure = [];
+//# sourceMappingURL=concatenation.js.map
